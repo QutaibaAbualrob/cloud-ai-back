3cb3283bdc08845321b665564532466d928518b8
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-05-16 20:11:16 +0300

    fixed the slash in vercel

--- aicloudproject/aicloudproject/settings.py
+++ aicloudproject/aicloudproject/settings.py
@@ -94,7 +94,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://localhost:8000",
-    "https://cloud-ai-front.vercel.app/",
+    "https://cloud-ai-front.vercel.app",
+    
 ]
 
 
