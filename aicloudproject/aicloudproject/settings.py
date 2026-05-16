6b4ed8b9becdd2d52ed30d7394537e8ce420d239
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-05-17 00:25:46 +0300

    added back sqllite

--- aicloudproject/aicloudproject/settings.py
+++ aicloudproject/aicloudproject/settings.py
@@ -162,12 +162,8 @@ WSGI_APPLICATION = 'aicloudproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'root',
-        'PASSWORD': 'cloudaiproject$',
-        'HOST': 'cloudaiproj.c23064s4m7e9.us-east-1.rds.amazonaws.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
